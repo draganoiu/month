4648ac7f32f507fd50af9140a46aafe498dc27e2
draganoiu <andrei_draganoiu@yahoo.com>
2016-12-15 12:32:51 -0800

    fara bugs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   
-before_action :authenticate_user!, except: [:index, :show]
+before_action :authenticate_user!, except: [:index]
   # GET /items
   def index
     @items = Item.all
